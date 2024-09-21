568207e79d6fe05420136f0381f11ae68246475b
Catalin NICULESCU <cn.cata@gmail.com>
2024-09-21 17:01:21 +0100

    Adjustments on the reward function

--- rl_gym_environments.py
+++ rl_gym_environments.py
@@ -31,23 +31,25 @@ class SUMOEnv(gym.Env):
         # Actions will be one of the following values [30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
         self.action_space = gym.spaces.Discrete(11)
         # self.actions_taken = []
-        self.observation_space = gym.spaces.Box(low=np.array([0]*5), high=np.array([1]*5), dtype="float32", shape=(5,))
+        self.observation_space = gym.spaces.Box(low=np.array([0]*4), high=np.array([1]*4), dtype="float32", shape=(4,))
         self.speed_limit = 130 # this is changed actively
         self.aggregation_time = 60 # [s] duration over which data is aggregated or averaged in the simulation
         # Simulation length for 24 hours (86400 seconds)
         self.sim_length = 24 * 60 * 60 / self.aggregation_time  # 60 x 1440 = 86400 steps
-        self.mean_speeds = []
-        self.flows = []
-        self.emissions_over_time = []
-        self.total_travel_time = 0
-        self.avg_total_travel_time = []
-        self.total_waiting_time = 0
-        self.avg_total_waiting_time = []
+        self.mean_speeds_mps = []
+        self.mean_emissions = []
+        self.mean_num_halts = []
+        # self.flows = []
+        # self.emissions_over_time = []
+        self.total_travel_time = 0 # [min]
+        # self.avg_total_travel_time = []
+        # self.total_waiting_time = 0
+        # self.avg_total_waiting_time = []
         self.sumo_process = None
         self.sumo_max_retries = 5
         self.day_index = 0
-        self.stop_counts = {} # Dictionary to track stop counts for each vehicle
-        self.previous_speeds = {}
+        # self.stop_counts = {} # Dictionary to track stop counts for each vehicle
+        # self.previous_speeds = {}
         # self.start_sumo()
     
     def start_sumo(self):
@@ -83,7 +85,40 @@ class SUMOEnv(gym.Env):
                 pass  # Ignore errors when closing
             self.sumo_process.terminate()
             self.sumo_process = None
-        
+
+    def track_vehicle_times(self, all_segments, departure_times, arrival_times, traci):
+        # Create a list of the keys to iterate over
+        departure_times_keys = list(departure_times.keys())
+        arrival_times_keys = list(arrival_times.keys())
+
+        for idx, seg in enumerate(all_segments):
+            for lane in seg:
+                ids = traci.lane.getLastStepVehicleIDs(lane)
+                
+                # Iterate over a copy of the keys
+                for id in departure_times_keys:
+                    if id not in ids:
+                        if id in arrival_times_keys:
+                            # Check if both keys exist before accessing them
+                            dep_time = departure_times.get(id)
+                            arr_time = arrival_times.get(id)
+                            if dep_time is not None and arr_time is not None:
+                                travel_time = max(0, arr_time - dep_time)
+                                self.total_travel_time += travel_time / 60
+                                # Remove entries from the original dictionaries
+                                del departure_times[id]
+                                del arrival_times[id]
+                                # logging.debug(f"Removed {id} from departure_times and arrival_times")
+
+                for id in ids:
+                    current_time = traci.simulation.getTime()
+                    if id in departure_times:
+                        arrival_times[id] = current_time
+                    else:
+                        departure_times[id] = current_time
+                        # logging.debug(f"Added {id} to departure_times with time {current_time}")
+                    self.total_waiting_time += traci.vehicle.getAccumulatedWaitingTime(id) # Calculate total waiting time for all vehicles still in simulation
+
     def step(self, action):
         is_sumo_running = psutil.pid_exists(self.sumo_process.pid) if self.sumo_process else False
         if not is_sumo_running:
@@ -95,17 +130,20 @@ class SUMOEnv(gym.Env):
         #     self.actions_taken.append(self.speed_limit)
 
         # copied from mtfc
-        speed_new = self.speed_limit / 3.6 # km/h to m/s
+        speed_new_mps = self.speed_limit / 3.6 # km/h to m/s
         road_segments = [seg_1_before]
 
         for segment in road_segments:
             if is_sumo_running:
-               [traci.lane.setMaxSpeed(lane, speed_new) for lane in segment]
+               [traci.lane.setMaxSpeed(lane, speed_new_mps) for lane in segment]
                 
-        mean_edge_speed = np.zeros(len(edges))
+        mean_speeds_edges_mps = np.zeros(len(edges))
+        num_halts_edges = np.zeros(len(edges))
+        emissions_edges = np.zeros(len(edges))
         veh_time_sum = 0        
         occupancy_sum = 0
-        departure_times = {}
+        # departure_times = {}
+        # arrival_times = {}
 
         for i in range(self.aggregation_time):
             try:
@@ -113,15 +151,15 @@ class SUMOEnv(gym.Env):
             except (FatalTraCIError, TraCIException):
                 logging.debug("Lost connection to SUMO. Attempting to reconnect...")
                 self.close_sumo("lost comm with SUMO")
-                return self.reset()[0]*5, 0, True, False, {}  # End episode on connection loss
+                return self.reset()[0]*4, 0, True, False, {}  # End episode on connection loss
 
             veh_time_sum += sum([traci.inductionloop.getLastStepVehicleNumber(loop) for loop in loops_after])
 
-            emission_sum = 0
-            for i, edge in enumerate(edges):
-                mean_edge_speed[i] += traci.edge.getLastStepMeanSpeed(edge)
-                emission_sum += traci.edge.getCO2Emission(edge)
-            self.emissions_over_time.append(emission_sum)
+            for idx, edge in enumerate(edges):
+                mean_speeds_edges_mps[idx] += traci.edge.getLastStepMeanSpeed(edge)
+                num_halts_edges[idx] += traci.edge.getLastStepHaltingNumber(edge)  # Number of vehicles stopped
+                emissions_edges += traci.edge.getCO2Emission(edge)
+            # self.emissions_over_time.append(emissions_edges)
          
             occ_max = 0
             for loops in loops_before:
@@ -131,24 +169,12 @@ class SUMOEnv(gym.Env):
 
             occupancy_sum += occ_max
 
-        arrived_vehicle_ids = traci.simulation.getArrivedIDList() # FIXME: return 0, does not work!
-        for i, seg in enumerate(all_segments):
-            for lane in seg:
-                ids = traci.lane.getLastStepVehicleIDs(lane)
-                for id in ids:
-                    departure_times[id] = traci.simulation.getTime()
-                    self.total_waiting_time += traci.vehicle.getAccumulatedWaitingTime(id) # Calculate total waiting time for all vehicles still in simulation
-                # Track arrived vehicles and calculate travel times
-                for id in arrived_vehicle_ids:
-                    if id in departure_times:
-                        arrival_time = traci.simulation.getTime()
-                        travel_time = arrival_time - departure_times[id]
-                        self.total_travel_time += travel_time
-                        # Remove vehicle from tracking
-                        del departure_times[id]
-                                    
-        self.avg_total_travel_time.append(self.total_travel_time / self.aggregation_time)
-        self.avg_total_waiting_time.append(self.total_waiting_time / self.aggregation_time)
+            # Granularity: lane | The following can be used for granularity: vehicle
+            # veh_in_collision = traci.simulation.getCollidingVehiclesNumber() # min 2 in case of one collision
+            # self.track_vehicle_times(all_segments, departure_times, arrival_times, traci)
+                                        
+        # self.avg_total_travel_time.append(self.total_travel_time / self.aggregation_time)
+        # self.avg_total_waiting_time.append(self.total_waiting_time / self.aggregation_time)
 
         # for i, seg in enumerate(all_segments):
         #     cvs_sum = 0
@@ -168,56 +194,68 @@ class SUMOEnv(gym.Env):
         #         cvs_seg = 0
         #     self.cvs_seg_time[i].append(cvs_seg)
 
-        mean_edge_speed = mean_edge_speed / self.aggregation_time # first is acutally a sum
-        mean_road_speed = sum(mean_edge_speed) / len(mean_edge_speed)
-        self.mean_speeds.append(mean_road_speed) 
+        mean_speeds_edges_mps = mean_speeds_edges_mps / self.aggregation_time
+        self.mean_speeds_mps.append(sum(mean_speeds_edges_mps) / len(mean_speeds_edges_mps)) 
 
-        flow = (veh_time_sum / self.aggregation_time) * 60 * 60 * 24
-        self.flows.append(flow)     
+        num_halts_edges = num_halts_edges / self.aggregation_time
+        self.mean_num_halts.append(sum(num_halts_edges)) 
+
+        emissions_edges = emissions_edges / self.aggregation_time
+        self.mean_emissions.append(sum(emissions_edges) / len(emissions_edges)) 
+
+        # flow = (veh_time_sum / self.aggregation_time)
+        # self.flows.append(flow)     
 
         # or Traffic Density: Represents the proportion of the road occupied by vehicles. 
         # It's a critical factor in understanding congestion levels. [based on traffic flow model]
         occupancy = occupancy_sum / self.aggregation_time
 
-        avg_emissions_over_time = np.mean(self.emissions_over_time)
-        if avg_emissions_over_time > max_emissions_over_time:
-            logging.debug(f"Avg emis. over {max_emissions_over_time}: {avg_emissions_over_time}")
+        # avg_emissions_over_time = np.mean(self.emissions_over_time)
+        # if avg_emissions_over_time > max_emissions_over_time:
+        #     logging.debug(f"Avg emis. over {max_emissions_over_time}: {avg_emissions_over_time}")
 
         # Normalize each component of the observation
-        normalized_mean_speed = normalize(mean_road_speed, min_value=0, max_value=130/3.6)
+        normalized_mean_speed = normalize(np.mean(self.mean_speeds_mps), min_value=0, max_value=130/3.6)
         normalized_occupancy = normalize(occupancy, min_value=0, max_value=100)
-        normalized_emissions = normalize(avg_emissions_over_time, min_value=0, max_value=max_emissions_over_time)
-        normalized_waiting_time = normalize(self.total_waiting_time, min_value=0, max_value=3600)
-        normalized_travel_time = normalize(self.total_travel_time, min_value=0, max_value=3600)
+        normalized_emissions = normalize(np.mean(self.mean_emissions), min_value=0, max_value=max_emissions)
+        normalized_halts = normalize(np.mean(self.mean_num_halts), min_value=0, max_value=10)
+        # normalized_waiting_time = normalize(self.total_waiting_time, min_value=0, max_value=3600)
+        # normalized_travel_time = normalize(self.total_travel_time, min_value=0, max_value=3600)
 
         # Construct normalized observation vector
         obs = np.array([
             normalized_mean_speed,
             normalized_occupancy,
             normalized_emissions,
-            normalized_waiting_time,
-            normalized_travel_time
+            normalized_halts
+            # normalized_waiting_time,
+            # normalized_travel_time
         ], dtype=np.float32)
         
-        reward = complex_reward(speed_new, mean_road_speed, occupancy, avg_emissions_over_time, self.total_waiting_time, self.total_travel_time)
+        reward = complex_reward(speed_new_mps, np.mean(self.mean_speeds_mps), occupancy, np.mean(self.mean_emissions), np.mean(self.mean_num_halts))
 
         # Reduce simulation length by 1 second
         self.sim_length -= 1 
         done = self.sim_length <= 0
         if done:
             logging.info(f"--------------------------------\nSUMO PID: {self.sumo_process.pid}")
-            # logging.info(f"CO2 emis All: {self.emissions_over_time}")
-            logging.info(f"CO2 emis Max: {max(self.emissions_over_time)}")
-            logging.info(f"CO2 emis Mean: {np.mean(self.emissions_over_time)}")
-            logging.info(f"CO2 emis Sum: {sum(self.emissions_over_time)}")
-            logging.info(f"Mean speed: {mean_road_speed}")
-            logging.info(f"Avg Mean speed (all scenarios): {np.mean(self.mean_speeds)}")
+
+            logging.info(f"Max CO2 emis: {max(self.mean_emissions)} mg/s")
+            logging.info(f"Avg CO2 emis: {np.mean(self.mean_emissions)} mg/s")
+
+            logging.info(f"Max Mean speed: {max(self.mean_speeds_mps)} m/s")
+            logging.info(f"Avg Mean speed: {np.mean(self.mean_speeds_mps)} m/s")
+
+            logging.info(f"Max No Halts: {max(self.mean_num_halts)}")
+            logging.info(f"Avg No Halts: {np.mean(self.mean_num_halts)}")
+
             logging.info(f"Occupancy Sum: {occupancy_sum}")
             logging.info(f"Occupancy/60: {occupancy}")
-            logging.info(f"TWT : {self.total_waiting_time / self.total_waiting_time}")
-            logging.info(f"Avg TWT : {np.mean(self.avg_total_waiting_time)}")
-            logging.info(f"TTT: {self.total_travel_time}")
-            logging.info(f"Avg TTT: {np.mean(self.avg_total_travel_time)}")
+
+            # logging.info(f"TWT : {self.total_waiting_time / self.total_waiting_time}")
+            # logging.info(f"Avg TWT : {np.mean(self.avg_total_waiting_time)}")
+            # logging.info(f"TTT: {self.total_travel_time}")
+            # logging.info(f"Avg TTT: {np.mean(self.avg_total_travel_time)}")
             # logging.info(f"Action: {self.actions_taken}\n--------------------------------")
 
             self.close_sumo("simualtion done")
@@ -231,16 +269,18 @@ class SUMOEnv(gym.Env):
     
     def reset(self, seed=None):
         self.day_index = 0
-        self.mean_speeds = []
-        self.flows = []
-        self.emissions_over_time = []
+        self.mean_speeds_mps = []
+        self.mean_emissions = []
+        self.mean_num_halts = []
+        # self.flows = []
+        # self.emissions_over_time = []
         self.total_travel_time = 0
         self.total_waiting_time = 0
         self.sumo_process = None
         self.sumo_max_retries = 5
         self.day_index = 0
-        self.stop_counts = {} # Dictionary to track stop counts for each vehicle
-        self.previous_speeds = {}
+        # self.stop_counts = {} # Dictionary to track stop counts for each vehicle
+        # self.previous_speeds = {}
 
         # Reset params
         self.speed_limit = 130
@@ -248,7 +288,7 @@ class SUMOEnv(gym.Env):
         # Reset time
         self.sim_length = 24 * 60 * 60 / self.aggregation_time
 
-        obs = np.array([0]*5, dtype=np.float32)
+        obs = np.array([0]*4, dtype=np.float32)
         
         return obs, {}
     
--- rl_utilities/reward_functions.py
+++ rl_utilities/reward_functions.py
@@ -1,4 +1,4 @@
-max_emissions_over_time = 1000000 # empirical data after running the simulation at max capacity
+max_emissions = 2000000 # empirical data after running the simulation at max capacity
 
 def normalize(value, min_value, max_value):
     """Normalize a value between min_value and max_value to a range [0, 1]."""
@@ -21,9 +21,9 @@ def quad_occ_reward(occupancy): # FIXME
         return 0
 
 # Minimize emissions by providing higher rewards for lower emission levels.
-def emission_reward(avg_emission, max_emission):
+def emission_reward(avg_emission):
     # Normalize emissions to a range [0, 1]
-    normalized_emission = min(avg_emission / max_emission, 1)
+    normalized_emission = min(avg_emission / max_emissions, 1)
     return 1 - normalized_emission  # Higher reward for lower emissions
 
 # Minimize total travel time by rewarding faster completion of trips.
@@ -32,46 +32,43 @@ def travel_time_reward(total_travel_time, max_travel_time=3600):
     normalized_travel_time = min(total_travel_time / max_travel_time, 1)
     return 1 - normalized_travel_time  # Higher reward for shorter travel times
 
+def num_halts_reward(num_halts, max_num_halts=10):
+    normalized_num_halts = min(num_halts / max_num_halts, 1)
+    return 1 - normalized_num_halts  # Higher reward for shorter travel times
+
 # Encourage speeds close to a target speed (e.g., optimal flow speed) while penalizing speeds that are too low or too high.
-def speed_reward(mean_speed, target_speed_mps):
+def speed_reward(mean_speed_mps, target_speed_mps):
     # Reward is maximum when mean speed is at target speed
-    if mean_speed < 0:
+    if mean_speed_mps < 0:
         return 0
-    elif mean_speed <= target_speed_mps:
-        return mean_speed / target_speed_mps
+    elif mean_speed_mps <= target_speed_mps:
+        return mean_speed_mps / target_speed_mps
     else:
         # Penalize speeds significantly higher than the target to ensure safety
-        return max(0, 1 - ((mean_speed - target_speed_mps) / target_speed_mps))
+        return max(0, 1 - ((mean_speed_mps - target_speed_mps) / target_speed_mps))
 
 def wait_time_penalty(total_waiting_time):
     # Penalize both total waiting time and number of stops
     return (-1) * (total_waiting_time / 3600)
 
-def complex_reward(target_speed_mps, mean_speeds, occupancy, avg_emission, total_waiting_time, total_travel_time):
-    weight_speed = 0.225
-    weight_occupancy = 0.225
-    weight_emissions = 0.225
-    weight_travel_time = 0.225
-    waiting_penalty_weight = 0.1
+def complex_reward(target_speed_mps, mean_speeds_mps, occupancy, mean_emissions, mean_num_halts):
+    weight_speed = 0.25
+    weight_occupancy = 0.25
+    weight_emissions = 0.25
+    weight_num_halts = 0.25
 
     # Calculate rewards for each parameter
-    spd_reward = speed_reward(mean_speeds, target_speed_mps)
-    emis_reward = emission_reward(avg_emission, max_emissions_over_time)
-    twt_penalty = wait_time_penalty(total_waiting_time)
+    spd_reward = speed_reward(mean_speeds_mps, target_speed_mps)
+    emis_reward = emission_reward(mean_emissions)
     occ_reward = quad_occ_reward(occupancy)
-    tts_reward = travel_time_reward(total_travel_time)
+    halts_reward = num_halts_reward(mean_num_halts)
 
     # Combine individual rewards into a total reward
     reward = (
         occ_reward * weight_occupancy +
         spd_reward * weight_speed +
         emis_reward * weight_emissions +
-        tts_reward * weight_travel_time +
-        twt_penalty * waiting_penalty_weight
+        halts_reward * weight_num_halts
     )
-    
-    # Normalize weights so they sum to 1 if needed
-    total_weight = weight_occupancy + weight_speed + weight_emissions + weight_travel_time
-    reward /= total_weight
 
     return reward
